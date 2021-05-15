86726ccddfd5fcb2e04b9f5203ebdc8ab33b68aa
Alex Keeley <keeley.lp@gmail.com>
2021-05-15 14:42:34 +0100

    Now displaying my list of songs through my songlist component and mapping each song in the array through a helper method to display through the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { selectSong } from "../actions/index";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList></SongList>
+    </div>
+  );
 };
 export default App;
