436cc6a57607904eda51c2787796b829513a27d5
Alex Keeley <keeley.lp@gmail.com>
2021-05-15 17:47:49 +0100

    Added songdetail component to display details of selecred song

---
+++ src/components/SongDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = (props) => {
+  if (!props.song) {
+    return <div>Select a song</div>;
+  }
+  return <div>{props.song.title}</div>;
+};
+
+const mapStateToProps = (state) => {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
